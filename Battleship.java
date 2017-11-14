242a60801c7e7c2cea31718041e8c4357c0b7cef
Tristan Crawford <xxtristan98xx@gmail.com>
2017-11-13 21:59:14 -0600

    Added addShip()

--- Battleship.java
+++ Battleship.java
@@ -5,105 +5,130 @@ import javax.swing.*;
 
 public class Battleship extends JFrame {
 
-	//Panels for Game
-	JPanel mainframe;
+	// Constants
+	public static final int BOARD_ROW = 10;
+	public static final int BOARD_COL = 10;
+	public static final int CELL_SIZE = 50;
 
-	//Attempts user makes
-	int attempts = 0;
-	
-	//Constants
-	public static final int BOARD_W = 7;
-	public static final int BOARD_H = 7;
-	public static final int CELL_WH = 64;
-
-	//Which cell the user attempts to attack
-	Cell target;
-	
-	//Array representing the playing grid
-	Cell cell_grid[][] = new Cell[BOARD_W][BOARD_H];
+	// Array representing the playing grid
+	private Cell cell_grid[][] = new Cell[ BOARD_ROW ][ BOARD_COL ];
 	
 	public Battleship() {
-		
-		this.setTitle("Battleship");
-		this.setSize(CELL_WH*BOARD_W,CELL_WH*BOARD_H);
-		this.getContentPane().setBackground(Color.BLACK);
 
-		this.setLayout(new GridLayout(BOARD_W,BOARD_H));
+		this.setTitle( "Battleship" );
+		this.setSize( CELL_SIZE * BOARD_COL, CELL_SIZE * BOARD_ROW );
+		this.getContentPane().setBackground( Color.BLACK );
+
+		this.setLayout( new GridLayout( BOARD_ROW,BOARD_COL ) );
 		
-		// Assigns Panels to GameFrame
-		for (int a = 0; a < BOARD_W; a++)
+		// Create Cell Grid
+		for ( int r = 0; r < BOARD_ROW; r++ )
         {
-            for (int b = 0; b < BOARD_H; b++)
+            for ( int c = 0; c < BOARD_COL; c++ )
             {   
-				cell_grid[a][b] = new Cell();
-                cell_grid[a][b].addMouseListener(new Listener(a,b));
-           		this.add(cell_grid[a][b]);
+				cell_grid[r][c] = new Cell();
+                cell_grid[r][c].addMouseListener( new Listener( r, c ) );
+           		this.add( cell_grid[r][c] );
         	}
-        } 
+        }
 		
-		this.setVisible(true);
+		// Add Ships To Random Cells
+		this.addShip( 1 );
+		this.addShip( 2 );
+		this.addShip( 3 );
+
+		this.setVisible( true );
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	
-	//Creates Specialized Cells For Battleship
-	public static class Cell extends JPanel
-	{
-		public static final Color BG_NORMAL = Color.GRAY;
-		public static final Color BG_HOVER  = Color.LIGHT_GRAY;
-		public static final Color BG_TARGET = Color.CYAN;
-		public static final Color BG_HIT    = Color.GREEN;
-		public static final Color BG_MISS   = Color.RED;
 
-		public Cell()
-		{
-			this.setOpaque(true); 
-			this.setBackground(BG_NORMAL);
-			this.setPreferredSize(new Dimension (Battleship.CELL_WH, Battleship.CELL_WH));
-			this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+	public void addShip( int size ) {
+
+		boolean dir = ( ( int )( Math.random() * 2 ) ) != 0; // 0 = Horizontal, 1 = Vertical
+		int start_a = ( ( int )( Math.random() * ( dir ? BOARD_COL : BOARD_ROW ) - 1 ) );
+		int start_b = ( ( int )( Math.random() * ( dir ? BOARD_ROW : BOARD_COL ) - 1 ) );
+
+		for ( int a = start_a; a < ( dir ? BOARD_COL : BOARD_ROW ); a++ )	{
+			for (int b = start_b; b < ( dir ? BOARD_ROW : BOARD_COL ); b++ ) {
+
+				boolean overlap = false;
+
+				for ( int c = 0; c < size; c++ ) {
+
+					boolean condition = ( dir ) ? ( cell_grid[a][b+c].isOccupied() )
+												: ( cell_grid[b][a+c].isOccupied() );
+
+					if ( condition ) {
+						overlap = true;
+						break;
+					}
+				}
+
+				if ( overlap ) continue;
+
+				for ( int c = 0; c < size; c++ ) {
+					
+					Cell to_occupy = ( dir ) ? ( cell_grid[a][b+c] )
+											 : ( cell_grid[b][a+c] );
+
+					to_occupy.occupy();
+				}
+
+				return;
+			}
 		}
 	}
-	
-	//Mouse Movements and Calculations
-	public class Listener implements MouseListener 
-	{
-		private int row; // stores the value of which row the panel that was pressed is in
-		private int col; // stores the value of which column the panel that was pressed is in
+
+	public static class Cell extends JPanel {
+
+		public static final Color BG_NORMAL = Color.GRAY;
+		public static final Color BG_HOVER  = Color.LIGHT_GRAY;
+		public static final Color BG_HIT    = Color.GREEN;
+		public static final Color BG_MISS   = Color.RED;
 		
-		public Listener(int row, int col)
-        {
-        	this.row = row;
-        	this.col = col;
+		private boolean occupied = false;
+
+		public Cell() {
+			this.setOpaque(true); 
+			this.setBackground(BG_NORMAL);
+			this.setPreferredSize(new Dimension (Battleship.CELL_SIZE, Battleship.CELL_SIZE));
+			this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		}
+		
+		// Visual Debugging
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.drawString( this.occupied ? "O" : "X", 10, 20);
+		}
+
+		public void occupy() { this.occupied = true; }
+		public boolean isOccupied() { return this.occupied;	}
+	}
+	
+	// Mouse Movements and Calculations
+	public class Listener implements MouseListener {
+
+		private Cell cell;
+
+		public Listener(int row, int col) {
+        	this.cell = cell_grid[row][col];
         }
 		
 		public void mouseClicked(MouseEvent e) {
-			switch (e.getButton())
-			{
-				// Left Mouse Button - Select Target
-				case MouseEvent.BUTTON1:
-					cell_grid[row][col].setBackground(Cell.BG_TARGET);
-					break;
-
-				// Right Mouse Button - Remove Target
-				case MouseEvent.BUTTON3:
-					cell_grid[row][col].setBackground(Cell.BG_NORMAL);
-					break;
-			}
-
-			// System.out.println(String.format("Clicked panel: [%d,%d]", row, col));
+			this.cell.setBackground(cell.isOccupied() ? Cell.BG_HIT : Cell.BG_MISS);
 		}
 
 		public void mouseEntered(MouseEvent e) {
-			if (cell_grid[row][col].getBackground() != Cell.BG_TARGET)
-				cell_grid[row][col].setBackground(Cell.BG_HOVER);
-
-			// System.out.println(String.format("Mouse entered panel: [%d,%d]", row, col));
+			if (this.cell.getBackground() != Cell.BG_HIT
+			&&  this.cell.getBackground() != Cell.BG_MISS) {
+				this.cell.setBackground(Cell.BG_HOVER);
+			}
 		}
 
 		public void mouseExited(MouseEvent e) {
-			if (cell_grid[row][col].getBackground() != Cell.BG_TARGET)
-				cell_grid[row][col].setBackground(Cell.BG_NORMAL);
-		
-			// System.out.println(String.format("Mouse left panel: [%d,%d]", row, col));
+			if (this.cell.getBackground() != Cell.BG_HIT
+			&&  this.cell.getBackground() != Cell.BG_MISS) {
+				this.cell.setBackground(Cell.BG_NORMAL);
+			}
 		}
 
 		public void mousePressed(MouseEvent e) {}
@@ -111,7 +136,7 @@ public class Battleship extends JFrame {
 		public void mouseReleased(MouseEvent e) {}
 	}
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		JFrame.setDefaultLookAndFeelDecorated(true);
 		new Battleship();
 	}
